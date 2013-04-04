55982a77d2257914379b250a5586ca9cfebf0676
Robin Kåveland Hansen <rkh@knowit.no>
2013-04-04 19:44:38 +0200

    Pointfree wc

--- workshop2-tc/wc/wc.hs
+++ workshop2-tc/wc/wc.hs
@@ -3,6 +3,7 @@ module Main(main) where
 import System.IO
 import System.Environment
 import Control.Applicative((<*>), pure)
+import Control.Monad
 import Data.List(intercalate)
 
 countingFunctions = [length . lines, length . words, length]
@@ -11,8 +12,7 @@ count :: String -> [Int]
 count = (countingFunctions <*>) . pure
 
 showCount = intercalate " " . map show . count
-
-wc f = f >>= hGetContents >>= return . showCount >>= putStrLn
+wc = (putStrLn =<<) . fmap showCount . (hGetContents =<<)
 
 main = do
   args <- getArgs
